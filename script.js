c12158fb50d4927a194d2ee1618e6a145a7746ff
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-11 19:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // your code here
+function updateUrl() {
+  var name = document.getElementById("name").value;
+  var year = document.getElementById("year").value;
+  document.getElementById("url").innerHTML = "https://localhost:8080/?name=" + name + "&year=" + year;
+}
