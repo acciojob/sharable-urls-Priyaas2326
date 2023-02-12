d8b7b3bfaf67d3dc0573b6dc9416dbfffb38f3bd
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-12 19:24:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// your code here
-function updateUrl() {
-  var name = document.getElementById("name").value;
-  var year = document.getElementById("year").value;
-  document.getElementById("url").innerHTML = "https://localhost:8080/?name=" + name + "&year=" + year;
-}
+const form = document.querySelector("form");
+    const nameInput = document.querySelector("#name");
+    const yearInput = document.querySelector("#year");
+    const urlDisplay = document.querySelector("#url");
+
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const name = nameInput.value;
+      const year = yearInput.value;
+      urlDisplay.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+    });
