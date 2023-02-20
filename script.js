5282646da4c30d18013a02b36b485bcd727a2ad8
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-20 21:24:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const form = document.querySelector("form");
-    const nameInput = document.querySelector("#name");
-    const yearInput = document.querySelector("#year");
-    const urlDisplay = document.querySelector("#url");
-
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      const name = nameInput.value;
-      const year = yearInput.value;
-      urlDisplay.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
-    });
+ const form = document.querySelector('form');
+      const url = document.querySelector('#url');
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const name = document.querySelector('#name').value;
+        const year = document.querySelector('#year').value;
+        if (name && year) {
+          const queryString = `?name=${name}&year=${year}`;
+          url.textContent = `https://localhost:8080/${queryString}`;
+        } else {
+          alert('Name and year cannot be empty');
+        }
+      });
